5143d7e14ed2ccbf513e838570549c9d3efb36a8
João Victor Rocha Milhomem <joaovictormilho@gmail.com>
2021-02-25 08:55:28 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const FRONT = 'card_front';
 const BACK = 'card_back';
+const CARD = 'card'
+const ICON = 'icon';
 
 let techs = [
     'bootstrap',
@@ -8,7 +10,7 @@ let techs = [
     'firebase',
     'html',
     'javascript',
-    'jquerry',
+    'jquery',
     'mongo',
     'node',
     'react'
@@ -22,6 +24,41 @@ function startGame() {
     cards = createCardsFromTechs(techs);
     shuffleCards(cards);
     console.log(cards);
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById('gameBoard');
+
+    cards.forEach(card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+        createCardContent(card, cardElement);
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    });
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if (face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./images/" + card.icon + '.png';
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = '&lt/&gt';
+    }
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards) {
@@ -72,4 +109,8 @@ function createIdsWithTech(tech) {
     }while(ids[0] === ids[1]);
 
     return ids;
+}
+
+function flipCard() {
+    this.classList.add('flip');
 }
